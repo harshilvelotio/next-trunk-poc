498ec9ea5e4b1a6bbf9d6b8809f788cff37a1c42
Harshil Shah <harshil.shah@velotio.com>
2021-12-01 12:40:10 +0530

    - Added environment based showing

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js! STAGING1</a>
+          Welcome to <a href="https://nextjs.org">Next.js! {process.env.EXAMPLE_NAME}</a>
         </h1>
 
         <p className={styles.description}>
